3f28b6d4d4a417b35d4fbb47f3ee64440cbd9a7e
Abbetor <balbin.bordsson@gmail.com>
2026-09-17 22:33:51 +0200

    Dubbelkolla felhantering med dubbleter m.m

--- runprogram.cs
+++ runprogram.cs
@@ -1,17 +1,17 @@
 Student albin = new Student("Albin");
 Student maja = new Student("Maja");
 Student erik = new Student("Erik");
-
+Student berit = new Student("Berit");
 Course matte = new Course("Matte");
 matte.MaxSeats = 2;
-
+berit.Leave(matte);// remove utan att programmet crashar
 albin.Join(matte);
 albin.Join(matte);
 maja.Join(matte);
-erik.Join(matte);
+erik.Join(matte); 
 
-matte.Rollcall();
-albin.Schedule();
+matte.Rollcall();// erik tas inte in eftersom det redan finns två personer
+albin.Schedule(); // Inga dubbleter albin x2
 
 albin.Leave(matte);
 matte.Rollcall();
